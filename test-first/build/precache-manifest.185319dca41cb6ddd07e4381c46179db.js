145ac1eb83c72a6fc803c4fdc1438d8c22c847c8
oleg <hnapovskiy20@gmail.com>
2019-09-10 18:30:27 +0300

    add map

---
+++ test-first/build/precache-manifest.185319dca41cb6ddd07e4381c46179db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74045bf9588bbeb85f12",
+    "url": "/static/js/main.697888ce.chunk.js"
+  },
+  {
+    "revision": "bb925ff48b846a8400a2",
+    "url": "/static/js/2.4bcdc629.chunk.js"
+  },
+  {
+    "revision": "74045bf9588bbeb85f12",
+    "url": "/static/css/main.a64038d4.chunk.css"
+  },
+  {
+    "revision": "e69d841603de62fdea4412ee5c698ca0",
+    "url": "/index.html"
+  }
+];
